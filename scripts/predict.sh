02b31eb38f469be57521b9bb65433c25be7d0136
WenchenLi <largemaths@gmail.com>
2017-07-16 21:26:15 -0500

    now integrated everything, start clean up and doc/readme

--- scripts/predict.sh
+++ scripts/predict.sh
@@ -7,5 +7,6 @@ WORD_EMBEDDING=data/word_embedding/wiki.zh.wang2vec.100.vec
 TRAIN_DATA=$WORKING_DIR/train.transitions
 TRAIN_CONLL_LEMMA=$1/train.conll.pb.lemmas
 PREDICT_DATA=example/predict_example.transitions
+#/home/wenchen/gpu/predicate_identification/input_to_joint_syn_sem_parser.predict
 #predict
 cmake-build-debug/parser/lstm-parse --predict -T $TRAIN_DATA --predict_data $PREDICT_DATA --model $MODEL -w $WORD_EMBEDDING --propbank_lemmas $TRAIN_CONLL_LEMMA -s dev.predictions.conll
